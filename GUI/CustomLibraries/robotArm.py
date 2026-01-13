f18acf52df660eb3aa786c84594757bf29639d7e
bangelooo <152049118+bangelooo@users.noreply.github.com>
2026-01-12 23:33:56 -0800

    Add methods to calculate spatial and body jacobians. Add description to RobotArm class methods
    Add methods to calculate spatial and body jacobians. Add description to RobotArm class methods

--- GUI/CustomLibraries/robotArm.py
+++ GUI/CustomLibraries/robotArm.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-class robotLink():
+class RobotLink():
     # Constructor
     def __init__(self,alpha,a,d,theta,jointType,dhType,*args):
         self.alpha = alpha
@@ -12,14 +12,15 @@ class robotLink():
         self.jointType = jointType
         self.dhType = dhType
 
-class robotArm():
+class RobotArm():
     # Constructor
     def __init__(self,links):
         # Class attributes
         self.links = links
         self.numLinks = len(links)
         self.jointPosition = np.zeros(len(links))
-        self.pose = self._initPose()
+        self.pose = self._getPoseDict()
+        self.jointTypes = self._getJointType()
         # Check to see that all links are of the same DH type. If not, throw an error.
         dhTypes = {link.dhType for link in links}
         if len(dhTypes) != 1:
@@ -29,6 +30,15 @@ class robotArm():
     # ============================================================
     #                       FORWARD KINEMATICS
     # ============================================================
+    def _getJointType(self):
+        """
+        Returns a tuple of type of joints of the robot (base -> EE)
+        """
+        jointTypeList = []
+        for link in self.links:
+            jointTypeList.append(link.jointType)
+        return tuple(jointTypeList)
+
     def calculateHT(self, linkNum, q):
         """
         Returns the homogeneous transform of the specified robot link
@@ -96,7 +106,12 @@ class robotArm():
         self.linkPos = jointTransforms
         return T, jointTransforms
 
-    def _getPoseDict(self,q):
+    def _getPoseDict(self):
+        """
+        Returns a dictionary of the robot pose.
+        Keys: "FK" , "POSITION", "ANGLE"
+        """
+        q = self.jointPosition
         FK , _ = self.forwardKin(q)
         r11 = FK[0][0]
         r21 = FK[1][0]
@@ -107,20 +122,41 @@ class robotArm():
             "ANGLE": yaw
             }
         return poseDict
-
-    def _initPose(self):
-        q = self.jointPosition
-        initPose = self._getPoseDict(q)
-        return initPose
     
-    def updateJoints(self,q):
-        self.jointPosition = q
-        poseDict = self._getPoseDict(q) # Update POSE dictionary of robot arm
-        self.pose = poseDict
+    def getJoints(self):
+        """
+        Returns a list of the current joint positions
+        """
+        return self.jointPosition
+
+    def updateJoints(self,moveType: str,q):
+        """
+        Updates the joint positions based on the move type.
+
+        :param moveType: "REL" or "ABS"
+        :type moveType: str
+        :param q: List of joint positions (in radians)
+        """
+        if moveType == "REL":
+            for jIndex,jValue in enumerate(self.jointPosition):
+                jValue += q[jIndex]
+        elif moveType == "ABS":
+            self.jointPosition = q
+        self.pose = self._getPoseDict() # Update POSE dictionary of robot arm
+    
     # ============================================================
     #                       INVERSE KINEMATICS
     # ============================================================
     def scaraIK(self,x,y,z,phi,elbowOrient):
+        '''
+        Returns a tuple of the joint angles required to achieve the desired robot pose.
+        
+        :param x: x-coordinate
+        :param y: y-coorindate
+        :param z: z-coordinate
+        :param phi: yaw-angle about the base frame
+        :param elbowOrient: Elbow orientation
+        '''
         # Extract link information of robot
         L = ["null"]
         d = ["null"]
@@ -175,29 +211,122 @@ class robotArm():
     # ============================================================
     #                       VELOCITY KINEMATICS
     # ============================================================
-    def jacob(self):
-        pass
+    def jacob0(self,whichJacobian: str = "J"):
+        """
+        Returns the Spatial Jacobian (Base Frame).
+        Input: "Jv" = Linear portion | "Jw" = "Angular"
+        """
+
+        # Linear and angular Jacobian matrices
+        Jv = np.zeros((3,self.numLinks))
+        Jw = np.zeros((3,self.numLinks))
+
+        # Get forward kinematics of current joint positions
+        FK,jointHT = self.forwardKin(self.jointPosition)
+        pEE = FK[:3,3] # End Effector Position
+
+        # Multiply rotation matrix by z0 to get z_i
+        z = np.array([0.0,0.0,1.0])
+        
+        # Base Frame HT is an identity matrix
+        T00 = np.eye(4)
+        R00 = T00[:3,:3]
+
+        # Set rotation matrix and position matrix for loop 
+        prevRotMat = R00
+        prevPosMat = T00[:3,3]
+
+        # Generate jacobian matrix
+        for i,HT in enumerate(jointHT):
+            if self.jointTypes[i] == "prismatic":
+                Jv[:,i] = prevRotMat @ z
+                Jw[:,i] = np.zeros(3)
+            elif self.jointTypes[i] == "revolute":
+                Jv[:,i] = np.cross(prevRotMat @ z, pEE - prevPosMat)
+                Jw[:,i] = prevRotMat @ z
+
+            # Iterate matrix
+            prevRotMat = HT[:3,:3]
+            prevPosMat = HT[:3,3]
+        
+        if whichJacobian == "Jv":
+            return Jv
+        elif whichJacobian == "Jw":
+            return Jw
+        else:
+            J = np.vstack((Jv,Jw))
+            return J
+
+    def jacobEE(self,whichJacobian:str = "J"):
+        # Extract Rotation Matrix (Base to EE)
+        R0E = self.pose["FK"][:3,:3]
+        
+        # Get Inverse
+        RE0 = np.linalg.inv(R0E)
+
+        # Get spatial jacobian (base frame)
+        J = self.jacob0("J")
+        Jv = J[:3,:]
+        Jw = J[3:,:]
+
+        if whichJacobian == "Jv":
+            return RE0 @ Jv
+        elif whichJacobian == "Jw":
+            return RE0 @ Jw
+        else:
+            return RE0 @ J
+        
+    def jacobBanSCARA(self):
+        """
+        Prints the spatial (base frame) and body (ee frame) jacobians.
+        Hand calculations.
+        """
+        L = [0]
+        for link in self.links:
+            L.append(link.a)
+        
+        theta = self.jointPosition
+        theta = np.insert(theta,0,0)
+
+        JvEE = np.zeros((3,4))
+
+        JvEE[0,0] = 0
+        JvEE[0,1] = L[2] * np.sin(theta[3] + theta[4]) + L[3] * np.sin(theta[4])
+        JvEE[0,2] = L[3] * np.sin(theta[4])
+        JvEE[0,3] = 0
+
+        JvEE[1,0] = 0
+        JvEE[1,1] = - (L[2] * np.cos(theta[3] + theta[4]) + L[3] * np.cos(theta[4]) + L[4])
+        JvEE[1,2] = - (L[3] * np.cos(theta[4]) + L[4])
+        JvEE[1,3] = - L[4]
+        
+        JvEE[2,0] = - 1
+        JvEE[2,1] = 0
+        JvEE[2,2] = 0
+        JvEE[2,3] = 0        
+        FK = self.pose["FK"]
+        R0E = FK[:3,:3]
+        Jv0 =  R0E @ JvEE
+
+        # Round
+        JvEE = np.round(JvEE,2)
+        Jv0 = np.round(Jv0,2)
+
+        print(f"Body Jacobian (EE Frame) Using Derivation:\n {JvEE}")
+        print(f"Spatial Jacobian(Base Frame) Using Derviation: \n {Jv0}")
+
     # ============================================================
     #                       ANIMATION 
     # ============================================================
-    def displayRobot(self):
-        x = [0]
-        y = [0]
-        z = [0]
-        for frame in self.linkPos:
-            x.append(frame[0,3].copy())
-            y.append(frame[1,3].copy())
-            z.append(frame[2,3].copy())
-
-        print(x)
-        plt.figure()
-        plt.plot(x[1:],y[1:],marker = 'o')
-        plt.grid(True)
-        plt.xlim(-3,3)
-        plt.ylim(-2,2)
-        plt.show()
 
     def animateTraj(self,X,phi,elbowOrient,sample):
+        """
+        Animate the trajectory (top-view) of the SCARA robot using a desired pose 
+        
+        :param X: List of start and end positions for (X,Y,Z). ie (0,1,0,2,0,3)
+        :param phi: Yaw about world frame
+        :param elbowOrient: Elbow Orientation
+        """
         xStart,xEnd = X[0][0], X[0][1]
         yStart,yEnd = X[1][0], X[1][1]
         zStart,zEnd = X[2][0], X[2][1]
